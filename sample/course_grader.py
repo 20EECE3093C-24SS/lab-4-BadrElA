0e30bcdc27733bc5a4620b513a89d22994bfced2
Badr <56516564+BadrElA@users.noreply.github.com>
2024-04-23 17:47:37 +0000

    course grader added

--- sample/course_grader.py
+++ sample/course_grader.py
@@ -1,4 +1,22 @@
 # TODO-1: add convert_to_letter_grade(score) function
     
-define convert_to_letter_grade(score):
+def convert_to_letter_grade(score):
+    
+    if score < 0 or score > 100:
+        raise ValueError("Score must be between 0 and 100.” for input scores outside the 0 to 100 range.")
+    elif type(score) != int or type(score) != float:
+        raise TypeError("Score must be a numeric value.")
+
+    if score < 60: 
+        return "F"
+    elif score < 70:
+        return "D"
+    elif score < 80:
+        return "C"
+    elif score < 90:
+        return "B"
+    else:
+        return "A"
+    
+
     
