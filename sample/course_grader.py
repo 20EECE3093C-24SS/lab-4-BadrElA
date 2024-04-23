656cffb521195ea6900f67e3fa027512b6817097
Badr <56516564+BadrElA@users.noreply.github.com>
2024-04-23 23:00:49 +0000

    peepee

--- sample/course_grader.py
+++ sample/course_grader.py
@@ -1,6 +1,31 @@
 # TODO-1: add convert_to_letter_grade(score) function
     
 def convert_to_letter_grade(score):
+    """Converts number grade to letter grade
+
+    Takes input of a grade number (int or float), between 0 and 100,
+    and converts it the corresponding letter grade
+
+    Args:
+        score: int or float from 0 to 100
+
+    Returns:
+        Letter string corresponding to letter grade.
+        For example:
+            A: 90 to 100
+            B: 80 to 89
+            C: 70 to 79
+            D: 60 to 69
+            F: Below 60
+
+        Return will always be a single, capital letter string.  If the
+        input is not an int/float or if the value is no within bounds (<0 or >100),
+        an error will be raised.
+
+    Raises:
+        TyperError: An error occurred due to inproper type (not an int or float).
+        ValueError: An error occuered due to the entered number being outside the 0 to 100 range
+    """
     
     if score < 0 or score > 100:
         raise ValueError("Score must be between 0 and 100.” for input scores outside the 0 to 100 range.")
--- tests/test_course_grader.py
+++ tests/test_course_grader.py
@@ -3,10 +3,39 @@ from course_grader import convert_to_letter_grade
 
 
 # TODO-1: Add test_exact_grade_boundaries() function
+def test_exact_boundaries():
+    assert convert_to_letter_grade(0) == 'F', "Score at 0 should be F"
+    assert convert_to_letter_grade(59) == 'F', "Score at 59 should be F"
+    assert convert_to_letter_grade(60) == 'D', "Score at 60 should be D"
+    assert convert_to_letter_grade(69) == 'D', "Score at 69 should be D"
+    assert convert_to_letter_grade(70) == 'C', "Score at 70 should be C"
+    assert convert_to_letter_grade(79) == 'C', "Score at 79 should be C"
+    assert convert_to_letter_grade(80) == 'B', "Score at 80 should be B"
+    assert convert_to_letter_grade(89) == 'B', "Score at 89 should be B"
+    assert convert_to_letter_grade(90) == 'A', "Score at 90 should be A"
+    assert convert_to_letter_grade(100) == 'A', "Score at 100 should be A"
 
 
 # TODO-2: Add test_invalid_numerical_score() function
-
+def test_invalid_numerical_score():
+    with pytest.raises(ValueError) as e:
+        convert_to_letter_grade(-1)
+    assert str(e.value) == "Score must be between 0 and 100."
+    
+    with pytest.raises(ValueError) as e:
+        convert_to_letter_grade(101)
+    assert str(e.value) == "Score must be between 0 and 100."
 
 # TODO-3: Add test_non_numeric_input() function
-
+def test_non_numeric_type():
+    with pytest.raises(TypeError) as e:
+        convert_to_letter_grade("eighty")
+    assert str(e.value) == "Score must be a numeric value. (String input)"
+    
+    with pytest.raises(TypeError) as e:
+        convert_to_letter_grade([90])
+    assert str(e.value) == "Score must be a numeric value. (List input)"
+    
+    with pytest.raises(TypeError) as e:
+        convert_to_letter_grade(None)
+    assert str(e.value) == "Score must be a numeric value. (None Input)"
