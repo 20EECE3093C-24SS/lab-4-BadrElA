991658d7d5cdbecfb1ab415aeb12be17bb93a2ed
Badr <56516564+BadrElA@users.noreply.github.com>
2024-04-23 23:19:49 +0000

    changed course grader

--- sample/course_grader.py
+++ sample/course_grader.py
@@ -1,7 +1,8 @@
 # TODO-1: add convert_to_letter_grade(score) function
     
 def convert_to_letter_grade(score):
-    """Converts number grade to letter grade
+    """
+    Converts number grade to letter grade
 
     Takes input of a grade number (int or float), between 0 and 100,
     and converts it the corresponding letter grade
@@ -25,23 +26,25 @@ def convert_to_letter_grade(score):
     Raises:
         TyperError: An error occurred due to inproper type (not an int or float).
         ValueError: An error occuered due to the entered number being outside the 0 to 100 range
+
+    Example:
+        >>> convert_to_letter_grade(64)
+        'D'
     """
     
-    if score < 0 or score > 100:
-        raise ValueError("Score must be between 0 and 100.” for input scores outside the 0 to 100 range.")
-    elif type(score) != int or type(score) != float:
+    if not isinstance(score, (int, float)):
         raise TypeError("Score must be a numeric value.")
+    if score < 0 or score > 100:
+        raise ValueError("Score must be between 0 and 100.”)
 
-    if score < 60: 
-        return "F"
-    elif score < 70:
-        return "D"
-    elif score < 80:
-        return "C"
-    elif score < 90:
-        return "B"
+    if score >= 90:
+        return 'A'
+    elif score >= 80 and score <= 89:
+        return 'B'
+    elif score >= 70 and score <= 79:
+        return 'C'
+    elif score >= 60 and score <= 69:
+        return 'D'
     else:
-        return "A"
-    
-
+        return 'F'
     
