e2077ccdc1025f99578fc53e96dcf0b79fedd0ad
Badr <56516564+BadrElA@users.noreply.github.com>
2024-04-23 23:43:46 +0000

    fixed course grader

--- sample/course_grader.py
+++ sample/course_grader.py
@@ -1,35 +1,29 @@
-# TODO-1: add convert_to_letter_grade(score) function
-    
 def convert_to_letter_grade(score):
     """
-    Converts number grade to letter grade
+    Converts a numerical grade to a corresponding letter grade.
 
-    Takes input of a grade number (int or float), between 0 and 100,
-    and converts it the corresponding letter grade
+    Takes input of a grade number (int or float) between 0 and 100,
+    and converts it to the corresponding letter grade.
 
     Args:
-        score: int or float from 0 to 100
+        score (int or float): Score from 0 to 100 to be converted to a letter grade.
 
     Returns:
-        Letter string corresponding to letter grade.
-        For example:
-            A: 90 to 100
-            B: 80 to 89
-            C: 70 to 79
-            D: 60 to 69
-            F: Below 60
+        str: Letter grade corresponding to the numeric score.
 
-        Return will always be a single, capital letter string.  If the
-        input is not an int/float or if the value is no within bounds (<0 or >100),
-        an error will be raised.
+    Examples:
+        A: 90 to 100
+        B: 80 to 89
+        C: 70 to 79
+        D: 60 to 69
+        F: Below 60
 
-    Raises:
-        TyperError: An error occurred due to inproper type (not an int or float).
-        ValueError: An error occuered due to the entered number being outside the 0 to 100 range
-
-    Example:
         >>> convert_to_letter_grade(64)
         'D'
+
+    Raises:
+        TypeError: If the input is not an int or float.
+        ValueError: If the score is outside the 0 to 100 range.
     """
     
     if not isinstance(score, (int, float)):
@@ -39,12 +33,11 @@ def convert_to_letter_grade(score):
 
     if score >= 90:
         return 'A'
-    elif score >= 80 and score <= 89:
+    elif score >= 80:
         return 'B'
-    elif score >= 70 and score <= 79:
+    elif score >= 70:
         return 'C'
-    elif score >= 60 and score <= 69:
+    elif score >= 60:
         return 'D'
     else:
         return 'F'
-    
